2ffd1de8c8d56471ee499e71cfcf487a2a0bf7da
ROMAN <roman@localhost.localdomain>
2023-08-22 15:33:14 +0300

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- * main-Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
+ * main-Write a function that takes a pointer to an 
+ * int as parameter 
+ * and updates the value it points to 98.
  * return - success
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
