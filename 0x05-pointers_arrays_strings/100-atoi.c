c21baf598c21459b21b8f47a0c8d24125ac86d63
Roman <108798583+Rockjunior@users.noreply.github.com>
2023-08-23 13:24:34 +0000

    10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *_atoi - Convert a string to an integer.
  *@s: The pointer to convert
@@ -12,21 +11,18 @@ int _atoi(char *s)
     unsigned int ni = 0;
     int min = 1;
     int isi = 0;
-
     while (s[c])
     {
         if (s[c] == 45)
         {
             min *= -1;
         }
-
         while (s[c] >= 48 && s[c] <= 57)
         {
             isi = 1;
             ni = (ni * 10) + (s[c] - '0');
             c++;
         }
-
         if (isi == 1)
         {
               break;
