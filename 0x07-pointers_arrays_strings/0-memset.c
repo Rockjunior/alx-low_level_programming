87c2364872aab2c8dcd94b5e372adb81f03a9513
Roman <108798583+Rockjunior@users.noreply.github.com>
2023-08-28 14:47:32 +0300

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * _memset=function fill thr first @n bytes of the area pointed
- * to @s with the constant byte @b
- * @n:bytes of the memory area pointed to by @s
- * @s:with the constant byte @b
- * @b:memory area pointer
- *
- * Return:a pointer to the memory area @s
- */
+* _memset - fill a block of memory with a specific value
+* @s: starting address of memory to be filled
+* @b: the desired value
+* @n: number of bytes to be changed
+*
+* Return: changed array with new value for n bytes
+*/
+
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int carrier = 0;
+int i = 0;
 
-while (carrier < n)
+for (; n > 0; i++)
 {
-s]carrier] = b;
-carrier++;
+s[i] = b;
+n--;
 }
-
 return (s);
 }
-/**Author:Roman*/
