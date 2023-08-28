5ce8b688526ca8f39f9da0e69d579d31fb572974
ROMAN <roman@localhost.localdomain>
2023-08-28 13:49:03 +0300

    8.Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_stirng-set value of pointer to a char
+ * @s:variable of type pointer in the system
+ * @to:char
+ * Return:void
+ */
+
+void set_string(char **s, char *to)
+{
+  *s = to;
+}
