1b959f38e60af611170d1cdc69fa7b5f1b41e29b
Roman <108798583+Rockjunior@users.noreply.github.com>
2023-08-28 15:34:24 +0300

    3.strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,30 +1,30 @@
 #include "main.h"
+
 /**
- * _strspn-return length of string that resembles values correctly
- * @s:string to search and the initial segment
- * @accept:target values which consist only of bytes
- *
- * Return:number of bytes target in values
- */
+* _strspn - Entry point
+* @s: input
+* @accept: input
+* Return: Always 0 (Success)
+*/
 
 unsigned int _strspn(char *s, char *accept)
 {
-  int number_i = 0, k;
-  int values = 0;
+	unsigned int n = 0;
+	int r;
 
-  while (s[number_i] != '\0')/*Iterate in string*/
-  {
-    for (k = 0; accept[k] != '\0'; k++)/*Iterate through target*/
-    {
-      if (s[number_i] == accept[k])/*record & break at first var*/
-      {
-        values++;
-        break;
-      }
-      if (accept[k+1} == '\0' && s[number_i] != accept[k])
-        return (values);
-  }
-  number_i++;
-}
-return (values);/*return all values if all are correct till end*/
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
 }
