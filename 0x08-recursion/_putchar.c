81f7efd7f0e35f35a91ca971de976f6d72ae6026
Roman <108798583+Rockjunior@users.noreply.github.com>
2023-08-30 23:14:10 +0300

    putchar

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -1,10 +1,12 @@
 #include <unistd.h>
+
 /**
  * _putcgar-writes the chararcter c to stdout
  * @c:the character to print
  * Return:success always
  * on error return -1 and error is set appropriately
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
