63c092b21173684f542c8317f59c3ba9d212dd8c
Roman <108798583+Rockjunior@users.noreply.github.com>
2023-08-30 22:40:41 +0300

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,24 +1,18 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
-* _puts_recursion - prints a string followed by a new line
-*@s:character to be printed
-*Return:returns void
-*/
+ * _puts_recursion - fills memory with a constant byte.
+ * @s: first bytes of the memory
+ * Return: -
+ */
 void _puts_recursion(char *s)
 {
-  if (*s == '\0')
-  {
-    putchar('\n');
-    return;
-  }
-  //print current xcter
-  putchar(*s);
-  _puts_recursion(s+1);
-}
-int main()
-{
-  char str[] = "Recursive,Functions";
-  _puts_recursion(str);
-  return (0);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
