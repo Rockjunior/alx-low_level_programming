d161dfde46dfbf9367d9b16326d370b0ccd121f4
ROMAN <roman@localhost.localdomain>
2023-09-04 19:19:41 +0300

    Now

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,23 +10,23 @@
 *
 * Return: a pointer to array, or NULL if it fails
 */
-
+char *create_array(unsigned int size, char c)
 {
-char *array;
-unsigned int newvalue;
+char *array;           /* declare a pointer to char 4 array*/
+unsigned int newvalue; /* declare a variable 2 hold current index*/
 
 if (size == 0)
-return (NULL);
+return (NULL);    /* return NULL if size = 0 */
 
-array = (char *)malloc(sizeof(char) * size); 
+array = (char *)malloc(sizeof(char) * size); /*allocate memory 4 array */
 if (array == NULL)
-return (NULL);
+return (NULL);    /*return NULL if memory allocation failed*/
 
-newvalue = 0;         
+newvalue = 0;         /*initialize index variable*/
 do {
-array[newvalue] = c; 
-newvalue++;          
-} while (newvalue < size); 
+array[newvalue] = c; /*set value of current element 2 c*/
+newvalue++;          /*increment index variable*/
+} while (newvalue < size); /*continue until all elements r initialized*/
 
 return (array);
 }
